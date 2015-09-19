ed92fd7d39b04b395af71cdf00ffd16a39207a02
mikeatgl <speakoftrees@gmail.com>
2015-09-19 13:14:21 -0400

    Now getting a single row or column.

--- sudoku.js
+++ sudoku.js
@@ -1,6 +1,5 @@
 var masterString = '...28.94.1.4...7......156.....8..57.4.......8.68..9.....196......5...8.3.43.28...';
 var masterArray = masterString.split('');
-var answersArray = [];
 var allPossible = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var possibilitiesArray = [];
 
@@ -70,8 +69,6 @@ function drawPuzzle(array) {
   console.log(rowBorder);
 }
 
-drawPuzzle(masterArray);
-
 // Get possibilites with basic deduction.
 
 function findPossibilites(array) {
@@ -79,31 +76,29 @@ function findPossibilites(array) {
     for (var j = 0; j < array[i].length; j++) {
       if (array[i][j] != ' ') {
         var answer = [array[i][j]];
-        array[i][j] = [array[i][j]];
         possibilitiesArray.push(answer);
       }
       else {
-        array[i][j] = [array[i][j]];
         possibilitiesArray.push(allPossible);
       }
     }
   }
+  console.log('Possibilities array: ');
   console.log(possibilitiesArray);
   console.log('Length of possibilites array: ' + possibilitiesArray.length);
   return possibilitiesArray;
 }
 
+
+
 // Eliminate possibilites by deduction using answers in row.
 
-function gatherAnswers(array) {
+function countAnswers(array) {
   var answerCount = 0;
   var unansweredCount = 0;
   for (var i = 0; i < array.length; i++) {
-    var rowAnswers = [];
     for (var j = 0; j < array[i].length; j++) {
       if (array[i][j] != ' ') {
-        var answer = array[i][j][0];
-        rowAnswers.push(answer);
         answerCount++;
       }
       else {
@@ -116,7 +111,49 @@ function gatherAnswers(array) {
   return array;
 }
 
-possibilitiesArray = findPossibilites(masterArray);
+function getRow(array, rowNum) {
+  if (rowNum < 1 || rowNum > 9) {
+    console.log('Your row number must be an integer between 1 and 9.');
+  }
+  else {
+    var thisRow = [];
+    for (var i = 0; i < 9; i++) {
+      thisRow.push(array[rowNum - 1][i]);
+    }
+    // console.log('Row ' + rowNum + ': ' + thisRow);
+    process.stdout.write('Row ' + rowNum + ': ');
+    console.log(thisRow);
+    return thisRow;
+  }
+}
 
-answersArray = gatherAnswers(masterArray);
+function getCol(array, colNum) {
+  if (colNum < 1 || colNum > 9) {
+    console.log('Your column number must be an integer between 1 and 9.');
+  }
+  else {
+    var thisCol = [];
+    for (var i = 0; i < array.length; i++) {
+      for (var j = 0; j < array[i].length; j++) {
+        if (j === (colNum - 1)) {
+          thisCol.push(array[i][j]);
+        }
+      }
+    }
+    process.stdout.write('Col ' + colNum + ': ');
+    console.log(thisCol);
+    return thisCol;
+  }
+}
+
+drawPuzzle(masterArray);
+
+findPossibilites(masterArray);
+
+countAnswers(masterArray);
+
+getRow(masterArray, 1);
+
+getCol(masterArray, 1);
+
 
